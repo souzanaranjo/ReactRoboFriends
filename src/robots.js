da3bb31f8c97011544e1d2395501022468cf43f3
David Souza <souzanaranjo@gmail.com>
2018-04-10 22:32:12 -0500

    Added the cards class

---
+++ src/robots.js
@@ -0,0 +1,31 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'David Souza',
+		username: 'davidsouza',
+		email: 'souzanaranjo@gmail.com'
+	},
+	{
+		id: 2,
+		name: 'Eduardo de la Garza',
+		username: 'eduardodlg',
+		email: 'delagarzaguerra@gmail.com'
+	},
+	{
+		id: 3,
+		name: 'Hernan Iruegas',
+		username: 'hernaniv',
+		email: 'iruegasvillarreal@gmail.com'
+	},
+	{
+		id: 4,
+		name: 'Daniel Vazquez',
+		username: 'danielv',
+		email: 'danielv@gmail.com'
+	},
+	{
+		id: 5,
+		name: 'Edgardo Leyva',
+		username: 'chupileyva',
+		email: 'chupileyva@gmail.com'
+	}]
