d887b103f2b5c59b2a5ff0b0020bb61b91432df4
David Souza <souzanaranjo@gmail.com>
2018-04-12 19:49:47 -0500

    Hardcoded some more friends into the robots.js file

--- src/robots.js
+++ src/robots.js
@@ -34,4 +34,40 @@ export const robots = [
 		name: 'Luis Rojo',
 		username: 'elrojo',
 		email: 'luisrojo@gmail.com'
+	},
+	{
+		id: 7,
+		name: 'Hector Moreno',
+		username: 'hectormoreno',
+		email: 'hectormoreno@gmail.com'
+	},
+	{
+		id: 8,
+		name: 'Sergio Loza',
+		username: 'checoloza',
+		email: 'checoloza@gmail.com'
+	},
+	{
+		id: 9,
+		name: 'Guillermo Mansur',
+		username: 'memomansur',
+		email: 'memomansur@gmail.com'
+	},
+	{
+		id: 10,
+		name: 'Javier Kuan',
+		username: 'javierkuan',
+		email: 'javierkuan@gmail.com'
+	},
+	{
+		id: 11,
+		name: 'German Villacorta',
+		username: 'germanvillacorta',
+		email: 'germanvillacorta@gmail.com'
+	},
+	{
+		id: 12,
+		name: 'Carlos Aldrete',
+		username: 'carlosaldrete',
+		email: 'carlosaldrete@gmail.com'
 	}]
