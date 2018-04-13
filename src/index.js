f7c4b770177cc14b617a2d8bce1f4b16b4466aef
David Souza <souzanaranjo@gmail.com>
2018-04-12 19:27:48 -0500

    Added the searchbar with full functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
-import { robots } from'./robots';
+import App from './App';
 
 ReactDOM.render(
-		<CardList robots={robots}/>
+		<App />
 	, document.getElementById('root'));
 registerServiceWorker();
